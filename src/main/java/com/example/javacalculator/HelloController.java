4209e45c004db09ec3766b27b16b00d7129876ba
Дима Бандуркин <dima1663@gmail.com>
2025-09-08 00:10:58 +0300

    для примера пойдёт

--- src/main/java/com/example/javacalculator/HelloController.java
+++ src/main/java/com/example/javacalculator/HelloController.java
@@ -2,8 +2,9 @@ package com.example.javacalculator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
+
 
 
 
@@ -12,70 +13,96 @@ public class HelloController {
     private TextField Zadacha;
     @FXML
     private TextField Otvet;
-    @FXML
-    Button btn;
-    private String _chslo = "0";
-    private String _chslo2 = "";
+
+    private String _chslo = "";
+
+    private double _otvet = 0;
+    private boolean Was = false;
+    private boolean Was2 = false;
+
+
+    private String _simvol = "";
+
 
     @FXML
-    protected void ClickOnAction(javafx.event.ActionEvent actionEvent) {
+    protected void ClickOnAction(/* тут мы прописываем объект, который вызывает событие как в шарпе sender*/javafx.event.ActionEvent actionEvent) {
         Button target = (Button) actionEvent.getTarget();
-        boolean Was = false;
-        switch (target.getText()) {
-            case "+":
-                if (Zadacha.getText() == "") {
-                    Zadacha.setText("0 + ");
-                } else {
+        if (!Was) {
+            switch (target.getText()) {
+                case "+":
+                    Was2 = false;
                     Zadacha.setText(Zadacha.getText() + " + ");
-                }
-                Otvet.setText(String.valueOf(Double.parseDouble(_chslo) + Double.parseDouble(_chslo2)));
-                Was = true;
-                break;
-            case "-":
-                if (Zadacha.getText() == "") {
-                    Zadacha.setText("0 - ");
-                } else {
+                    _simvol = "+";
+                    _chslo = "";
+                    break;
+                case "-":
+                    Was2 = false;
                     Zadacha.setText(Zadacha.getText() + " - ");
-                }
-                Otvet.setText(String.valueOf(Double.parseDouble(_chslo) - Double.parseDouble(_chslo2)));
-                Was = true;
-                break;
-            case "*":
-                if (Zadacha.getText() == "") {
-                    Zadacha.setText("0 * ");
-                } else {
+                    _simvol = "-";
+                    _chslo = "";
+                    break;
+                case "*":
+                    Was2 = false;
                     Zadacha.setText(Zadacha.getText() + " * ");
-                }
-                Otvet.setText(String.valueOf(Double.parseDouble(_chslo) * Double.parseDouble(_chslo2)));
-                Was = true;
-                break;
-            case "/":
-                if (Zadacha.getText() == "") {
-                    Zadacha.setText("0 / ");
-                } else {
+                    _simvol = "*";
+                    _chslo = "";
+                    break;
+                case "/":
+                    Was2 = false;
                     Zadacha.setText(Zadacha.getText() + " / ");
-                }
-                Otvet.setText(String.valueOf(Double.parseDouble(_chslo) / Double.parseDouble(_chslo2)));
-                Was = true;
-                break;
-            case ",":
-                if (Zadacha.getText() == "") {
-                    Zadacha.setText("0,");
-                } else {
-                    Zadacha.setText(Zadacha.getText() + ",");
-                }
-                break;
+                    _simvol = "/";
+                    _chslo = "";
+                    break;
+                case ",":
+                    if (!Was2) {
+                        Zadacha.setText(Zadacha.getText() + ",");
+                        String sim = _simvol;
+                        _simvol = "-";
+                        _otvet = Double.parseDouble(Razchet(String.valueOf(_otvet), _chslo));
+                        _simvol = sim;
+                        Was2 = true;
+                        _chslo += ".";
+                    }
+                    break;
+                case "=":
+                    if (Was2)
+                        _otvet = (Double.parseDouble(Razchet(String.valueOf(_otvet), _chslo)));
+                    Otvet.setText(String.valueOf(_otvet));
+                    Was = true;
+                    break;
+                case "^":
+                    Was2 = false;
+                    Zadacha.setText(Zadacha.getText() + " ^ ");
+                    _simvol = "^";
+                    _chslo = "";
+                    break;
+
+                case "C":
+                    Zadacha.setText("");
+                    Was = false;
+                    Was2 = false;
+                    _chslo = "";
+                    _otvet = 0;
+                    _simvol = "";
+                    break;
+
+            }
         }
-        if (Was) {
-            _chslo = Otvet.getText();
-            _chslo2 = "";
-        }
+
     }
 
     @FXML
     protected void ClickOnNumber(javafx.event.ActionEvent ationEvent) {
         Button target = (Button) ationEvent.getTarget();
         String str = Zadacha.getText();
+        if (Was) {
+            Zadacha.setText("");
+            Was = false;
+            Was2 = false;
+            _chslo = "";
+            _otvet = 0;
+            _simvol = "";
+        }
         try {
             if (str.charAt(str.length() - 1) != '=') {
                 Zadacha.setText(Zadacha.getText() + target.getText());
@@ -85,10 +112,38 @@ public class HelloController {
         } catch (Exception exception) {
             Zadacha.setText(Zadacha.getText() + target.getText());
         } finally {
-            _chslo2 += target.getText();
+            _chslo += target.getText();
+            if (!Was2) {
+                _otvet = Double.parseDouble(Razchet(String.valueOf(_otvet), _chslo));
+            }
+
         }
     }
 
+    private String Razchet(String chislo1, String chislo2) {//функция расчета
+        String rezultat;
+        switch (_simvol) {
+            case "+":
+                rezultat = String.valueOf(Double.parseDouble(chislo1) + Double.parseDouble(chislo2));
+                break;
+            case "-":
+                rezultat = String.valueOf(Double.parseDouble(chislo1) - Double.parseDouble(chislo2));
+                break;
+            case "*":
+                rezultat = String.valueOf(Double.parseDouble(chislo1) * Double.parseDouble(chislo2));
+                break;
+            case "/":
+                rezultat = String.valueOf(Double.parseDouble(chislo1) / Double.parseDouble(chislo2));
+                break;
+            case "^":
+                rezultat = String.valueOf(Math.pow(Double.parseDouble(chislo1) ,Double.parseDouble(chislo2) ));
+                break;
+            default:
+                return chislo2;
+        }
+        return rezultat;
+
+    }
 }
 
 
